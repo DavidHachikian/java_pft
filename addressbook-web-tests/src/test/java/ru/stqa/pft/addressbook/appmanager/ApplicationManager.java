67b3c1899820df10cb17c32ece576b3c4614def4
DavidHachikian <davidhachikian@gmail.com>
2020-11-10 18:34:20 +0300

    Исправление ошибок в коде (лекции 2.5-2.7)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationManager {
+  public WebDriver wd;
+
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  JavascriptExecutor js;
+  private Map<String, Object> vars;
+
+  public void init() {
+    wd = new FirefoxDriver();
+
+    wd.get("http://localhost/addressbook/");
+    wd.manage().window().setSize(new Dimension(896, 868));
+
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
+    js = (JavascriptExecutor) wd;
+    vars = new HashMap<String, Object>();
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+
+
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SessionHelper {
+  private WebDriver wd;
+
+  public SessionHelper(WebDriver wd) {
+
+    this.wd = wd;
+  }
+
+  public void login(String username, String password) {
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.cssSelector("input:nth-child(7)")).click();
+  }
+}
