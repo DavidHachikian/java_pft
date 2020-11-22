1967fb18d275f0c565528181eabe3642b308a876
DavidHachikian <davidhachikian@gmail.com>
2020-11-22 17:35:22 +0300

    правки в Helpbase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
@@ -20,10 +21,10 @@ public class HelperBase {
 
   protected void type(By locator, String text) {
     click(locator);
-
     if (text != null) {
       String existingText = wd.findElement(locator).getAttribute("value");
       if (! text.equals(existingText)) {
+        wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
       }
     }
@@ -31,9 +32,9 @@ public class HelperBase {
 
   protected boolean isElementPresent(By locator) {
     try {
-      wd.findElement(locator);
+      wd.switchTo().alert();
       return true;
-    } catch (NoSuchElementException ex) {
+    } catch (NoAlertPresentException e) {
       return false;
     }
 
