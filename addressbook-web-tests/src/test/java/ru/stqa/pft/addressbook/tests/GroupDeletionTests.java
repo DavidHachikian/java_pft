65d309170d909cef5afbd4a6118767633a189c24
DavidHachikian <davidhachikian@gmail.com>
2021-03-15 15:57:06 +0300

    Задание 15 (группы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -5,7 +5,8 @@ import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
@@ -17,8 +18,8 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.junit.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
-  //@Ignore
-  @Before
+
+  @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().all().size() == 0) {
@@ -26,14 +27,13 @@ public class GroupDeletionTests extends TestBase {
     }
   }
 
-  //@Ignore
   @Test
   public void groupDeletionTests() {
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
     assertThat(app.group().count(),equalTo(before.size() - 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(deletedGroup)));
   }
 
