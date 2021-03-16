b5f3c933e5242fc73b56631044c0b30c53a91efb
DavidHachikian <davidhachikian@gmail.com>
2021-03-16 17:02:42 +0300

    Лекция 7.5. Отключаемые проверки UI (contacts)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -31,7 +31,9 @@ public class ContactModificationTests extends TestBase {
     Contacts after = app.db().contacts();
     assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
+    verifyContactListInUI();
   }
+
 /*
   @Test
   public void testModifyContactSorted() {
