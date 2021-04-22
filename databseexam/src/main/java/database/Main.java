6f8fdd1acbf86fa3c7976d10471a3827f0cb852f
DavidHachikian <davidhachikian@gmail.com>
2021-04-22 17:38:59 +0300

    Тестовое задание (JDBC)

--- databseexam/src/main/java/database/Main.java
+++ databseexam/src/main/java/database/Main.java
@@ -1,16 +1,39 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.sql.ResultSet;
 
 public class Main {
   private static final String URL = "jdbc:mysql://localhost:3307/mydbtest?autoReconnect=true&useSSL=false";
   private static final String USERNAME = "root5678";
   private static final String PASSWORD = "root5678";
 
+  private static final String SALARY_SUM = "select DEPARTMENT, SUM(SALARY) as sum from employees group by DEPARTMENT";
+
+
   public static void main(String[] args) {
+    registerDriver();
+
+    Connection connection = null;
+    Statement preparedStatement = null;
+    try {
+      Driver driver;
+      driver = new com.mysql.cj.jdbc.Driver();
+      DriverManager.registerDriver(driver);
+      connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+      preparedStatement = connection.prepareStatement(SALARY_SUM);
+      ResultSet res = preparedStatement.executeQuery(SALARY_SUM);
+
+      while (res.next())
+      System.out.println(res.getString("DEPARTMENT") + ":" + " " + res.getFloat("sum"));
+      preparedStatement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  private static void registerDriver() {
     Connection connection;
     try {
       Driver driver;
@@ -18,6 +41,7 @@ public class Main {
       DriverManager.registerDriver(driver);
 
       connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
       if (!connection.isClosed()) {
         System.out.println("Соединение с БД установлено");
       }
@@ -26,6 +50,8 @@ public class Main {
       System.err.println("Соединение с БД не установлено");
     }
   }
+  
 }
+
 
 
